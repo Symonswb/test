852c2eb621a81393a525346c973d9fbcd168143a
symons1012 <symons1012@gmail.com>
2015-02-03 18:06:25 -0500

    Made new mytestcookbook folder

---
+++ cookbooks/mytestcookbook/metadata.rb
@@ -0,0 +1,9 @@
+name             'mytestcookbook'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures mytestcookbook'
+long_description 'Installs/Configures mytestcookbook'
+
+version          '0.1.0'
+
